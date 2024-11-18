1034ef5fdd1f147f592c0c4dc597410117ee7929
Alberto Russo <albertorusso1990@gmail.com>
2024-11-18 21:45:31 +0100

    removed useless test data

--- app/src/main/java/com/albertorusso/mytodo/data/datasource/DefaultTodoProvider.kt
+++ app/src/main/java/com/albertorusso/mytodo/data/datasource/DefaultTodoProvider.kt
@@ -5,8 +5,16 @@ import com.albertorusso.mytodo.data.model.TodoEntity
 object DefaultTodoProvider {
     fun getDefaultTodos(): List<TodoEntity> {
         return listOf(
-            TodoEntity(1, "Give an initial call", "For the candidate Alberto Russo, I should make an initial call to get to know him and see if he is a good fit for this role", true),
-            TodoEntity(2, "Send assignment", "For the candidate Alberto Russo, I should send him a technical assignment to see if he is able to deliver what we require", false),
+            TodoEntity(
+                1,
+                "Give an initial call",
+                "For the candidate Alberto Russo, I should make an initial call to get to know him and see if he is a good fit for this role",
+                true
+            ),
+            TodoEntity(
+                2, "Send assignment", "For the candidate Alberto Russo, I should send him a technical assignment to see if he is able to " +
+                        "deliver what we require", true
+            ),
             TodoEntity(3, "Make hiring decision", "For the candidate Alberto Russo, I should decide if I should hire him", false)
         )
     }
